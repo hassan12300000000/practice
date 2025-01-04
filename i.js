466e72a5ba615839f869c6081ee0c4182c255063
hassan <xsaala@gmail.com>
2025-01-04 13:28:38 +0300

    initial commit

---
+++ i.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById("menuToggle").addEventListener("click", function() {
+    const navLinks = document.getElementById("navLinks");
+    navLinks.classList.toggle("show");
+});
